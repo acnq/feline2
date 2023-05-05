6352dd1d7e3de6dbcc7c58e736f2c87fa50fd195
acnq <fanyuanhaotc@163.com>
2023-05-05 13:40:15 +0300

    init commit

---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open Lake DSL
+
+package feline2 {
+  -- add package configuration options here
+}
+
+@[default_target]
+lean_exe feline2 {
+  -- add executable configuration options here
+}
